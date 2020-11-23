7e3adabb8730fb4a664379ab893556f01682f61c
sameeksharathi <rathisameeksha9@gmail.com>
2020-11-23 16:57:52 +0530

    updated

--- Weapon and Fire Detection/weapon and Fire detection.py
+++ Weapon and Fire Detection/weapon and Fire detection.py
@@ -11,7 +11,7 @@ args, unknown = parser.parse_known_args()
 def load_yolo():
     net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
     classes = []
-    with open("obj.names", "r") as f:
+    with open("../Files/obj.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
 
     layers_names = net.getLayerNames()
